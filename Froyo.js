a238a44c9020ca81b9a7db4a6ecc9a1dade673f7
Justt-Mel <justinmelendezj@gmail.com>
2025-03-13 02:10:08 -0400

    added comments to code to inprove understanding of what the code does

--- Froyo.js
+++ Froyo.js
@@ -1,18 +1,23 @@
 // Prompt to take the Froyo order.
 const orderPrompt = prompt ("Please enter your FROYO order (include comma's Please): ");
 let orderList = orderPrompt.split(",");
+// Created object in order to take the input values of the prompt
 const inputOr= {};
+//printed out array to show that input was stored
 console.log(orderList);
-
+// For loop to copy array to the object defined above
     for(i = 0; i < orderList.length; i++)
     {
+        // New varible created to hold the value of the flavors ordered 
         const flavor = orderList[i];
+        //if statement created so that if a flavor has yet to be defined it can create an object for it
         if (inputOr[flavor] === undefined)
         {
             inputOr[flavor] = 0;
         }
         inputOr[flavor]++
-        console.log(inputOr)
     }
-
+    //printed out objext to show data was properly stored
+    console.log(inputOr)
+// prints out the table for for the inputOr Objects
     console.table(inputOr)
